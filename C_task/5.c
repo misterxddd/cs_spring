5879dadd7283dac6703e495c38fe2dceb351e124
Dima <gip840@mail.ru>
2017-03-20 20:33:06 +0300

    Create 5.c

---
+++ C_task/5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(0, "russian");
+	float C, F, t1, t2, m;
+	printf("Укажите диапозон в формате t1-t2 и шаг m: ");
+	scanf("%f-%f %f", &t1, &t2, &m);
+	C = t1;
+	do
+	{
+		F = (1.8*C) + 32;
+		printf("%f\t %f\n", C, F);
+		C = C + m;
+	} while (C <= t2);
+
+		
+	_getch();
+	return 0;
+}
