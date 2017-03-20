cde54bc447a3f81933426679e0d70192b91cca5f
Dima <gip840@mail.ru>
2017-03-20 21:03:52 +0300

    Create 18.c

---
+++ C_task/18.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+
+main(int argc, char * argv[] )
+{
+	setlocale(0, "russian");
+	if (argc != 3)
+	{
+		printf("До свидания!");
+			exit(1);
+	}
+	FILE *syrs;
+	FILE *sors;
+	long fsize;
+	char * buffer;
+	syrs = fopen(argv[1], "rb");
+	if (syrs == NULL)
+	{
+		printf("До свидания!");
+		exit(1);
+	}
+	fseek(syrs, 0, SEEK_END);
+	fsize = ftell(syrs);
+	rewind(syrs);
+
+	buffer = (char *)malloc(fsize);
+	fread(buffer, 1, fsize, syrs);
+	sors = fopen(argv[2], "wb");
+	
+	fwrite(buffer, 1, fsize, sors);
+
+	fclose(sors);
+	fclose(syrs);
+	free(buffer);
+	_getch();
+	return 0;
+}
