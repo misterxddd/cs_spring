25406d5b679b5855d8f80b2335ef8587030e0e79
Dima <gip840@mail.ru>
2017-03-20 20:53:04 +0300

    Create 10.c

---
+++ C_task/10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <locale.h>
+
+unsigned int rightrot(unsigned int x,unsigned int n)
+{
+	for(unsigned int i = 0; i < n; i++) 
+		__asm ror x, 1
+	return x;
+}
+
+int main()
+{
+	setlocale(0, "russian");
+	unsigned int x, n;
+	printf("Введите значение x и количество разрядов n: ");
+	scanf("%u %u", &x, &n);
+	x = rightrot(x, n);
+	printf("%u", x);
+	_getch();
+	return 0;
+}
