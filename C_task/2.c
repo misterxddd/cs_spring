a72b12c6512b6ce309622bd0afbb299971c200e8
Dima <gip840@mail.ru>
2017-03-20 20:30:12 +0300

    Create 2.c

---
+++ C_task/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int day = 7, month = 12;
+	int a = day;
+	printf("0%i.%i\n", day, month);
+	day = month;
+	month = a;
+	printf("%i.0%i", day, month);
+	getchar();
+	return 0;
+}
