a63d7ac5bc48103710fa81feb17e34df835175de
Dima <gip840@mail.ru>
2017-03-20 20:58:26 +0300

    Create 13.c

---
+++ C_task/13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	char mogy[100];
+	int per = 0;
+
+	gets(mogy);
+	for (int i = 0; i < 100; i++)
+	{
+		if ( (mogy[i] == ' ') || (mogy[i] == '\0') )
+		{
+			for (int c = 0; c < per; c++)
+				printf("-");
+			printf("\n");
+			for (int c = 0; c < per - 1; c++)
+				printf(" ");
+			printf("|\n");
+			for (int c = 0; c < per; c++)
+				printf("-");
+			printf("\n");
+			per = 0;
+		}
+		else
+			per++;
+		if (mogy[i] == '\0')
+			break;
+	}
+	_getch();
+	return 0;
+}
