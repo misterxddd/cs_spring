9582322322960f14e739612504e005608528bb86
Dima <gip840@mail.ru>
2017-03-23 20:54:04 +0300

    Update 16.c

--- C_task/16.c
+++ C_task/16.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <conio.h>
 #include <locale.h>
 
+int i = 0;
 
 char strncpy(char * su, char * ty, int x)
 {
-	for (int i = 0; i < x; i++)
+	for (i = 0; i < x; i++)
 	{
 		su[i] = ty[i];
 	}
@@ -14,12 +14,12 @@ char strncpy(char * su, char * ty, int x)
 char strncat(char * su, char * ty, int x)
 {
 	int count = 0;
-	for (int i = 0; i < 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		if (su[i] != '\0') count++;
 		else break;
 	}
-	for (int i = 0; i < x; i++)
+	for (i = 0; i < x; i++)
 	{
 		su[i + count] = ty[i];
 	}
@@ -28,7 +28,7 @@ char strncat(char * su, char * ty, int x)
 
 int strncmp(char * su, char * ty, int x)
 {
-	for (int i = 0; i < x; i++)
+	for (i = 0; i < x; i++)
 	{
 		if (su[i] != ty[i]) return 0;
 	}
@@ -38,13 +38,13 @@ int strncmp(char * su, char * ty, int x)
 int main()
 {
 	setlocale(0, "russian");
-	int x ;
+	int x, n ;
 	char ty[100], su[50];
-	for (int n = 0; n < 100; n++)
+	for (n = 0; n < 100; n++)
 	{
 		ty[n] = '\0';
 	}
-	for (int n = 0; n < 50; n++)
+	for (n = 0; n < 50; n++)
 	{
 		su[n] = '\0';
 	}
@@ -56,7 +56,6 @@ int main()
 	printf("Ваша строка c количеством символов n: %s\n", su);
 	strncat(su, ty, x);
 	printf("Строка из предыдущего действия с добавлением n символов к концу строки: %s\n", su);
-	printf("Строка из предыдущего действия с добавлением n символов к концу строки и исходная строка равны (если 1, то равны): %i", strncmp(su, ty, x));
-	_getch();
+	printf("Строка из предыдущего действия с добавлением n символов к концу строки и исходная строка равны (если 1, то равны): %i\n", strncmp(su, ty, x));
 	return 0;
 }
