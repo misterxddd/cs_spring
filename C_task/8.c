db0643b399af03dde6174e367a315171968bb901
Dima <gip840@mail.ru>
2017-03-23 22:02:48 +0300

    Update 8.c

--- C_task/8.c
+++ C_task/8.c
@@ -11,13 +11,13 @@ int main()
 	D = b*b - 4 * a * c;
 	if (D > 0)
 	{
-		x1 = (-b + sqrt(D)) / 2 * a;
-		x2 = (-b - sqrt(D)) / 2 * a;
+		x1 = (-b + sqrt(D)) / (2 * a);
+		x2 = (-b - sqrt(D)) / (2 * a);
 		printf("x1 = %f\nx2 = %f", x1, x2);
 	}
 	else if (D == 0)
 	{
-		x1 = x2 = -b / 2 * a;
+		x1 = x2 = -b / (2 * a);
 		printf("x1 = x2 = %f", x1);
 	}
 	else
