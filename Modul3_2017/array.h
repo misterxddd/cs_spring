936181dd9f823cd38490608c5d30e4ef26e3f607
Dima <gip840@mail.ru>
2017-04-27 11:01:35 +0300

    Update array.h

--- Modul3_2017/array.h
+++ Modul3_2017/array.h
@@ -6,6 +6,8 @@ typedef struct pop
 	struct pop * next;
 }string;
 
+enum functions { end, a, b, c, d, e, f, g, h, i, j, new };
+
 string * CreateString(char * str);
 string * CreateStringStruct(char _symbol, string * _next);
 void PrintString(string * _string);
@@ -17,7 +19,7 @@ void AddToTheEnd(string * _string, char _symbol);
 void AddNewItem(string * _string, int _index, char _symbol);
 void DeleteItem(string * _string, int _index);
 void DeleteLast(string * _string);
-void DeleteAll(string * _string);
+void DeleteAll(string * _string, int _lenght);
 void Sort(string * _string, void(*function)(char*, char*));
 void Sex(string * _string1, string * _string2);
 
